e54d73073e5ab7c478f7de09d0a89f6e6010850c
Kagwi <kelvinmwas8@gmail.com>
2026-09-14 04:11:01 -0700

    Update ABOUT_IMAGE URL to new staff image

--- src/data.ts
+++ src/data.ts
@@ -117,7 +117,7 @@ export const COLD_ROOM_IMAGE =
   'https://github.com/Kagwi/Kyms-Refrigeration/blob/main/Coldroom.jpeg?raw=true';
 
 export const ABOUT_IMAGE =
-  'https://images.pexels.com/photos/5463575/pexels-photo-5463575.jpeg?auto=compress&cs=tinysrgb&h=650&w=940';
+  'https://github.com/Kagwi/Kyms-Refrigeration/blob/main/Kyms%20Staff.jpeg?raw=true';
 
 export const PRODUCTS_IMAGE_BG =
   'https://images.pexels.com/photos/28169591/pexels-photo-28169591.jpeg?auto=compress&cs=tinysrgb&h=650&w=940';
