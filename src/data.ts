5bcab6d34bc8d4026e1f2d4a3a3809671eb31eea
Kagwi <kelvinmwas8@gmail.com>
2026-09-16 06:06:56 -0700

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -143,7 +143,7 @@ export const COLD_ROOM_IMAGE =
   'https://github.com/Kagwi/Kyms-Refrigeration/blob/main/Coldroom.jpeg?raw=true';
 
 export const ABOUT_IMAGE =
-  'https://github.com/Kagwi/Kyms-Refrigeration/blob/main/Kyms%20Staff.jpeg?raw=true';
+  'https://github.com/Kagwi/Kyms-Refrigeration/blob/main/Paul%20Kyms%20Staff.jpeg?raw=true';
 
 export const PRODUCTS_IMAGE_BG =
   'https://images.pexels.com/photos/28169591/pexels-photo-28169591.jpeg?auto=compress&cs=tinysrgb&h=650&w=940';
